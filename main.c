89291858be76d68c3e5b2656afc2712ec7d8b6f8
haythm elmagraby <haythmelmagraby@gmail.com>
2023-12-28 10:01:45 +0000

    everything work untill now

--- main.c
+++ main.c
@@ -8,24 +8,13 @@ int main(int ac,char **av){
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t read;
-	char* delim = " ";
-	
-//	char* str = copy_string(*av);	
-	char* str = copy_string("haythm ahmed aly abdallah");	
-	char* token = strtok(str,delim);
-	printf("%s\n",token);
-	while(token != NULL){ 
-	token = strtok(NULL,delim);
-	printf("%s\n",token);
-	}
-	printf("end tokens loop\n");
 
 	while(av[i] != NULL){	
-	printf("start args loop\n");
+//	printf("start args loop\n");
 		printf("index is [%d] %s\n",i,av[i]);
 		i++;
 	}
-	printf("finish args loop\n");
+//	printf("finish args loop\n");
 
 	printf("$");
 	while((read = getline(&line,&len,stdin)) != -1){
